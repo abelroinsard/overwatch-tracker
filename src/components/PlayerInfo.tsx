8e36f0da93830151d95bb26db858395466823aac
abel.roinsard <abel.roinsard@epita.fr>
2025-08-28 13:01:19 +0200

    feat: add play times

--- src/components/PlayerInfo.tsx
+++ src/components/PlayerInfo.tsx
@@ -8,6 +8,28 @@ export type PlayerInfoProps = {
 };
 
 export default function PlayerInfo({ playerInfo }: PlayerInfoProps) {
+  const getCareerStats = (platform: any, mode: "quickplay" | "competitive") => {
+    if (!platform || !platform[mode] || !platform[mode].career_stats || !platform[mode].career_stats["all-heroes"]) {
+      return 0;
+    }
+    
+    const statsArr = platform[mode].career_stats["all-heroes"];
+    
+    // Trouver la catégorie "game"
+    const gameCategory = statsArr.find((category: any) => category.category === "game");
+    if (!gameCategory || !gameCategory.stats) {
+      return 0;
+    }
+    
+    // Trouver le stat "time_played"
+    const timePlayedStat = gameCategory.stats.find((stat: any) => stat.key === "time_played");
+    return timePlayedStat ? timePlayedStat.value : 0;
+  };
+  const QPPCTime = getCareerStats(playerInfo.stats.pc, "quickplay");
+  const QPConsoleTime = getCareerStats(playerInfo.stats.console, "quickplay");
+  const CompPCTime = getCareerStats(playerInfo.stats.pc, "competitive");
+  const CompConsoleTime = getCareerStats(playerInfo.stats.console, "competitive");
+  
   return (
     <div className="PlayerInfo">
       <Summary
@@ -17,7 +39,7 @@ export default function PlayerInfo({ playerInfo }: PlayerInfoProps) {
         compConsole={playerInfo.summary.competitive.console}
         compPc={playerInfo.summary.competitive.pc}
       />
-      <div className="bottomInfo"><PlayTimes QPTime={0} CompTime={0}></PlayTimes></div>
+      <div className="bottomInfo"><PlayTimes QPTime={QPPCTime + QPConsoleTime} CompTime={CompPCTime + CompConsoleTime}></PlayTimes></div>
     </div>
   );
 }
--- src/components/playerInfo/PlayTimes.tsx
+++ src/components/playerInfo/PlayTimes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./PlayTimes.css";
-import { BarChart } from "@mui/x-charts";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 export type PlayTimesProps = {
   QPTime: number;
@@ -8,18 +8,30 @@ export type PlayTimesProps = {
 };
 
 export default function PlayTimes({ QPTime, CompTime }: PlayTimesProps) {
+  const QPPercentage = QPTime > CompTime ? 100 : (QPTime / CompTime) * 100;
+  const CompPercentage = CompTime > QPTime ? 100 : (CompTime / QPTime) * 100;
+  const QPHours = (QPTime / 3600).toString().split(".")[0] + " HOURS";
+  const CompHours = (CompTime / 3600).toString().split(".")[0] + " HOURS";
+  console.log(QPTime);
+
   return (
     <div className="PlayTimes">
       <h3>Play Times:</h3>
-      <div className="chart">
-        <BarChart
-          series={[
-            { data: [10, 20], label: "pv", id: "pvId" },
-            { data: [30, 40], label: "uv", id: "uvId" },
-          ]}
-          xAxis={[{ data: ["a", "b"] }]}
-          yAxis={[{ width: 50 }]}
-        />
+      <div className="playtimecontainers">
+        <div className="labels">
+          <div>QUICKPLAY</div>
+          <div>COMPETITIVE</div>
+        </div>
+        <div className="charts">
+          <ProgressBar
+            completed={QPPercentage}
+            customLabel={QPHours}
+          ></ProgressBar>
+          <ProgressBar
+            completed={CompPercentage}
+            customLabel={CompHours}
+          ></ProgressBar>
+        </div>
       </div>
     </div>
   );

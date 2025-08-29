ec6aa46845978913d221a197095d309069b080c1
abel.roinsard <abel.roinsard@epita.fr>
2025-08-29 09:40:28 +0200

    fix: change label

--- src/components/playerInfo/PlayTimes.tsx
+++ src/components/playerInfo/PlayTimes.tsx
@@ -10,8 +10,8 @@ export type PlayTimesProps = {
 export default function PlayTimes({ QPTime, CompTime }: PlayTimesProps) {
   const QPPercentage = QPTime > CompTime ? 100 : (QPTime / CompTime) * 100;
   const CompPercentage = CompTime > QPTime ? 100 : (CompTime / QPTime) * 100;
-  const QPHours = (QPTime / 3600).toString().split(".")[0] + " HOURS";
-  const CompHours = (CompTime / 3600).toString().split(".")[0] + " HOURS";
+  const QPHours = (QPTime / 3600).toString().split(".")[0] + "h";
+  const CompHours = (CompTime / 3600).toString().split(".")[0] + "h";
   console.log(QPTime);
 
   return (
